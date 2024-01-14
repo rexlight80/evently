f5dbe938619b3d8fb03f19de45c3f9b01cb95851
rexlight80 <indranajay@gmail.com>
2024-01-14 21:15:10 +0530

    second commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -9,6 +9,9 @@ export async function POST(req: Request) {
  
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+
+  console.log('HereApi');
+
  
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
@@ -51,10 +54,14 @@ export async function POST(req: Request) {
   }
  
   // Get the ID and type
-  const { id } = evt.data;
+  
   const eventType = evt.type;
  
+  console.log('EventType', evt.type);
+
   if(eventType == 'user.created'){
+  console.log('EventData', evt.data);
+
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
@@ -76,7 +83,7 @@ export async function POST(req: Request) {
          })
     }
 
-    return NextResponse.json({ message: 'OK' });
+    return NextResponse.json({ message: 'OK' , user: newUser});
   }
 
   if (eventType === 'user.updated') {
